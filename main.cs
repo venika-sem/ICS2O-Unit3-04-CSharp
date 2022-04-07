99fe177965236032eca3281cc8e065a87b9b6387
venika-sem <99354917+venika-sem@users.noreply.github.com>
2022-04-07 16:34:36 +0000

    changed main.cs

--- main.cs
+++ main.cs
@@ -1,7 +1,36 @@
+// Created by: Venika Sem
+// Created on: Apr 2022
+//
+// This program converts the temperature from fahrenheit to celsius.
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function converts the temperature from fahrenheit to celsius.
+        double fahrenheitTemperature;
+        double temperature;
+
+        // input
+        Console.WriteLine("This program converts the temperature from Fahrenheit to Celsius.");
+        Console.WriteLine("");
+        Console.WriteLine("The formula is: (°F − 32) × 5 / 9");
+        Console.WriteLine("");
+        Console.WriteLine("Please enter the degree in Fahrenheit (°F).");
+
+        Console.WriteLine("");
+        Console.Write("Input temperature in Fahrenheit: ");
+        fahrenheitTemperature = Convert.ToDouble(Console.ReadLine());
+
+        // process
+        Console.WriteLine("");
+        temperature = (fahrenheitTemperature - 32) * 5 / 9;
+
+        // output
+        Console.WriteLine("The temperature in Celcius is: " + temperature.ToString("0.00") + " " + "°C.");
+
+        Console.WriteLine("\nDone.");
+    }
 }
